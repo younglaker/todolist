2f10238ffce582786b36d42b047a592e231527f4
younglaker <younglaker8@gmail.com>
2017-01-19 17:25:26 +0800

    add todo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <index></index>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Index from './components/index'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Index
   }
 }
 </script>
---
+++ src/components/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="main">
+    <h1>{{ title }}</h1>
+
+    <input type="text" class="input_todo"
+        v-model="new_item"
+        v-on:keyup.enter="addNew()">
+
+    <ul>
+        <li v-for="item in items"
+            v-bind:class="{finished: item.is_finished}"
+            @click="toggleFinish(item)">
+            {{ item.label }}
+        </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'index',
+  data () {
+    return {
+        title: 'Hello Vue!',
+        items: [],
+        new_item: ''
+    }
+  },
+  methods: {
+  
+        toggleFinish: function (item) {
+            item.is_finished = !item.is_finished
+        },
+        addNew: function () {
+            this.items.push({
+                label: this.new_item,
+                is_finished: false
+            })
+            this.new_item = ''
+        }
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
