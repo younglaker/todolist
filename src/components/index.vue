66daaf76da2688d5e03f23cfc5b433cf1893eeee
younglaker <younglaker8@gmail.com>
2017-02-06 11:39:32 +0800

    `Del Todos` functin

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,21 +1,32 @@
 <template>
-    <div id="main" class="row">
-        <div class="col-md-8 col-sm-12 col-md-offset-2">
-                    
-            <input type="text" class="input_todo col-md-12"
-                v-model="new_item"
-                v-on:keyup.enter="addNew()">
-            
-            <ul>
-                <li v-for="item in items"
-                    v-bind:class="{finished: item.is_finished}"
-                    @click="toggleFinish(item)">
-                    {{ item.label }}
-                </li>
-            </ul>
-            
-          </div>
-      </div>
+    <div id="main" class="container">
+        <div class="row">
+            <div class="col-md-8 col-sm-12 col-md-offset-2">
+                <div class="row">
+                    <input type="text" class="input_todo col-md-12 form-control input-lg"  placeholder="Input something"
+                                v-model="new_item"
+                                v-on:keyup.enter="addNew()">
+                </div>
+
+                <div class="row">
+                    <ul class="list-group">
+                    <!-- v-for 还支持一个可选的第二个参数为当前项的索引 -->
+                        <li class="list-group-item"
+                            v-for="(item, index) in items"
+                            v-bind:class="{finished: item.is_finished}">
+                             <input type="checkbox" 
+                                    @click="toggleFinish(item)">
+                            {{ item.label }}
+                            <button class="destroy btn btn-danger btn-sm"
+                                @click="removeTodo(index)">X</button>
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -50,6 +61,9 @@ export default {
                 is_finished: false
             })
             this.new_item = ''
+        },
+        removeTodo: function(index) {
+            this.items.splice(index, 1)
         }
 
     }
@@ -64,3 +78,4 @@ export default {
 }
 
 </style>
+s
